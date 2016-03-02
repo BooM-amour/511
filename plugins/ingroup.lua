0b0b99586ba313ef1746afd791b242df89d0df29
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-02 04:57:16 -0800

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -1401,7 +1401,7 @@ return {
 }
 end
 
---Copyright and edit; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
+--Copyright and edit; @Sdfvbh
+--Persian Translate; @Sdfvbh
+--ch : @zvirusx
 --کپی بدون ذکر منبع حرام است--
